f520e0a4ace346dbc5c6775d828262eec5539937
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 21:51:18 +0200

    Update with new properties and objects to work with states

--- app/types/index.ts
+++ app/types/index.ts
@@ -6,12 +6,22 @@ export interface Movie {
     overview: string;
 }
 
+export interface MovieRated extends Movie {
+    rating: number;
+}
+
 export interface MovieState {
     movies: Movie[];
+    ratedMovies: MovieRated[];
     searchQuery: string;
     selectedMovie: Movie | null;
+    guestSessionId: string | null;
+
     setSearchQuery: (query: string) => void;
     setSelectedMovie: (movie: Movie | null) => void;
     setMovies: (movies: Movie[]) => void;
-    
+    initGuestSession: () => Promise<void>;
+    rateMovie: (movieId: number, rating: number) => Promise<void>;
+    getRatedMovies: () => Promise<void>;
+
 }
