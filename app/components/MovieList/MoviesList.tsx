01488d509ef6d72a858dad8f8e6a778b3ecbb91a
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 16:08:35 +0200

    Update state management in the component along react query

--- app/components/MovieList/MoviesList.tsx
+++ app/components/MovieList/MoviesList.tsx
@@ -1,46 +1,45 @@
+'use client'
+
 import { Movie } from "@/app/types"
 import Image from "next/image";
-import { getPopularFilms } from "@/app/lib/api"
+import { getPopularFilms, searchMovies } from "@/app/lib/api"
 import { useQuery } from '@tanstack/react-query'
+import { useFilmStore } from "@/app/store/movieStore"
 
-type Props = {
-    onMovieClick: (movie: Movie) => void;
-  };
+export const MoviesList = () => {
+  const { searchQuery, setSelectedMovie, setMovies } = useFilmStore();
 
+  const { data: movies, isLoading, error } = useQuery<Movie[], Error>({
+    queryKey: ['movies', searchQuery],
+    queryFn: () => searchQuery ? searchMovies(searchQuery) : getPopularFilms(),
+  });
 
-export const MoviesList = ({onMovieClick} : Props) => {
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
 
-    const { data: movies, isLoading, error } = useQuery<Movie[], Error>({
-        queryKey: ['popularMovies'],
-        queryFn: getPopularFilms
-      });
-
-      if (isLoading) return <div>Loading...</div>;
-      if (error) return <div>An error occurred: {error.message}</div>;
-
-      return ( 
-            <div className="w-full mx-auto">
-                {movies && (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                        {movies.map(movie => (
-                            <div key={movie.id} onClick={() => onMovieClick(movie)} className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer">
-                                <div className="relative h-64">
-                                    <Image
-                                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                        layout="fill"
-                                        objectFit="cover"
-                                        alt={`Poster of ${movie.title}`}
-                                    />
-                                </div>
-                                <div className="p-4">
-                                    <h2 className="text-xl font-semibold mb-2">{movie.title}</h2>
-                                        <p className="text-gray-600 text-sm mb-2">{movie.release_date}</p>
-                                        <p className="text-gray-800 text-sm line-clamp-3">{movie.overview}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                )}
+  return (
+    <div className="w-full mx-auto">
+      {movies && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {movies.map(movie => (
+            <div key={movie.id} onClick={() => setSelectedMovie(movie)} className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer">
+              <div className="relative h-64">
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  layout="fill"
+                  objectFit="cover"
+                  alt={`Poster of ${movie.title}`}
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mb-2">{movie.title}</h2>
+                <p className="text-gray-600 text-sm mb-2">{movie.release_date}</p>
+                <p className="text-gray-800 text-sm line-clamp-3">{movie.overview}</p>
+              </div>
             </div>
-      );
-}
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
