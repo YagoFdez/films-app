ccb680d9753d935be35907740a9f66d8564a8379
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 12:03:08 +0200

    Add queryClient provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+'use client'
+
 import Image from "next/image";
+import { Movies } from "./components/Movies/Movies";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 export default function Home() {
+  const queryClient = new QueryClient()
   return (
+    <QueryClientProvider client={queryClient}>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>Films</h1>
+        <Movies/>
       </div>
     </main>
+    </QueryClientProvider>
   );
 }
