e2341520d204d2151b7debe7e8016a36eb9c86e4
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 16:11:45 +0200

    Add state management when changing the search

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,19 +1,29 @@
 'use client'
 
-import { useState } from "react"
+import { useFilmStore } from "@/app/store/movieStore"
+import { useQueryClient } from '@tanstack/react-query'
 
-export const SearchBar = () =>  {
+export const SearchBar = () => {
+    
+  const queryClient = useQueryClient();
+  const { searchQuery, setSearchQuery } = useFilmStore();
 
-    const [query, setQuery] = useState('')
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    queryClient.invalidateQueries({ queryKey: ['movies'] });
+  };
 
-    return (
-        <div>
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
         <input
-            type="text"
-            value={query}
-            placeholder="Buscar pelicula..."
-            className="w-full p-2 border rounded-lg"
-            />
-            </div>
-    );
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Buscar pelicula..."
+          className="w-full p-2 border rounded-lg"
+        />
+      </form>
+    </div>
+  );
 }
