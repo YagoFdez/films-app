053d2f887439d42367391ab463df3e04ac7ca4df
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 16:06:42 +0200

    Update home with zustand store state and change the way we manage the components behaviour with it

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,26 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import { Movie } from "./types";
-import { useState } from "react";
 import { MovieDetail } from "./components/MovieDetails/MovieDetails";
 import { SearchBar } from "./components/SearchBar/SearchBar";
+import { useFilmStore } from "./store/movieStore";
 
 export default function Home() {
   const queryClient = new QueryClient()
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
-
-  const handleMovieClick = (movie : Movie) => {
-    setSelectedMovie(movie);
-  }
-
-  const handleCloseModal = () => {
-    setSelectedMovie(null)
-  }
+  const { selectedMovie, setSelectedMovie } = useFilmStore();
 
   return (
     <QueryClientProvider client={queryClient}>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <h1>Films</h1>
-                <SearchBar/>
-                <MoviesList onMovieClick={handleMovieClick} />
-                {selectedMovie && (
-                    <MovieDetail movieInfo={selectedMovie} onClose={handleCloseModal} />
-                )}
-            </div>
-        </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <h1>Films</h1>
+          <SearchBar/>
+          <MoviesList />
+          {selectedMovie && (
+            <MovieDetail movieInfo={selectedMovie} onClose={() => setSelectedMovie(null)} />
+          )}
+        </div>
+      </main>
     </QueryClientProvider>
   );
 }
