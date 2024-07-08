f5abdb38c8d20628ff499d319daf98e33ec04b81
YagoFdez <yago.fmiranda@gmail.com>
2024-07-08 13:15:11 +0200

    Show modal with specific info when click on a film

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,37 @@
 'use client'
 
-import Image from "next/image";
-import { Movies } from "./components/Movies/Movies";
+import { MoviesList } from "./components/MovieList/MoviesList";
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { Movie } from "./types";
+import { useState } from "react";
+import { MovieDetail } from "./components/MovieDetails/MovieDetails";
 
 export default function Home() {
   const queryClient = new QueryClient()
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleMovieClick = (movie : Movie) => {
+    setSelectedMovie(movie);
+  }
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null)
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Films</h1>
-        <Movies/>
-      </div>
-    </main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+                <h1>Films</h1>
+                <MoviesList onMovieClick={handleMovieClick} />
+                {selectedMovie && (
+                    <MovieDetail movieInfo={selectedMovie} onClose={handleCloseModal} />
+                )}
+            </div>
+        </main>
     </QueryClientProvider>
   );
 }
